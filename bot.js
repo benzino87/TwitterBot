2b3fc3d2b2e9b511291e841973c29e7c8511ed36
benzinofam <benzinofam@debianHomeLaptop>
2017-01-28 11:59:14 -0500

    basic tweet queries and posts working

--- bot.js
+++ bot.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+var Twit = require('twit');
+var tokens = require('./node_modules/tokens');
+
+var T = new Twit(tokens);
+
+var topics = {
+  q: 'Trump',
+  count: 5
+};
+
+
+T.get('search/tweets', topics , getData);
+
+
+function getData(err, data, response){
+  console.log(data);
+};
+
+var status = {
+  status: 'Hello! first tweet from tweetbot'
+};
+ 
+T.post('statuses/update', status, printData);
+
+function printData(err, data, response){
+  console.log(data);
+};
